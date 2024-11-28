52a872d870fd1e636e284c1e4833b83359544d6c
amoncabudo <amoncayobudo@cendrassos.net>
2024-11-28 19:37:07 +0100

    Fix addmachine

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./node_modules/flowbite-react/**/*.js",
-=======
-
-export default {
-  content: [
-    "./node_modules/flowbite-react/**/*.js",
 module.exports = {
-  content: ["./node_modules/flowbite/**/*.js"
+  content: [
+    "./node_modules/flowbite-react/**/*.js",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [  
+  plugins: [
     require('flowbite/plugin')
-  ],
-=======
-}
   ]
 }
