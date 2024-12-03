99c44bb94b516851ba4a7df57644efa03a45c94c
pbosch <pbosch@cenddrassos.net>
2024-12-03 10:36:14 +0100

    Maquetacion Admin Panel

--- App/Views/layouts/navbar.php
+++ App/Views/layouts/navbar.php
@@ -141,6 +141,18 @@
                                 <div class="px-4 py-3 text-sm text-gray-900">
                                     <div>Hola, <?= $_SESSION["user"]["name"] ?></div>
                                 </div>
+                             
+                                    <ul class="py-2 text-sm text-gray-700">
+                                        <li>
+                                            <a href="/adminPanel" class="flex items-center px-4 py-2 hover:bg-gray-100">
+                                                <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path>
+                                                </svg>
+                                                Panel de Administración
+                                            </a>
+                                        </li>
+                                    </ul>
+                             
                                 <ul class="py-2 text-sm text-gray-700">
                                     <li>
                                         <a href="/userconfig" class="flex items-center px-4 py-2 hover:bg-gray-100">
