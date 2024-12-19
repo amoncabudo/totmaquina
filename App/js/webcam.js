0fc4c09d9f472fb96f42306226b4fe14ec04db8e
pbosch <pbosch@cenddrassos.net>
2024-12-19 18:04:07 +0100

    Documentacion JS

--- App/js/webcam.js
+++ App/js/webcam.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const canvas = document.getElementById('canvas');
     const captureButton = document.getElementById('capture-photo');
     
+    // Check if required elements are present
     if (!video || !canvas || !captureButton) {
         console.warn('Required elements for webcam functionality not found');
         return;
@@ -10,21 +11,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const context = canvas.getContext('2d');
 
+    // Event listener for the capture button
     captureButton.addEventListener('click', async () => {
         if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
             try {
+                // Access the webcam
                 const stream = await navigator.mediaDevices.getUserMedia({ video: true });
                 video.srcObject = stream;
                 video.classList.remove('hidden');
-                captureButton.textContent = 'Tomar Foto';
+                captureButton.textContent = 'Take Photo';
                 
+                // Change the button functionality to capture the photo
                 captureButton.onclick = () => {
                     context.drawImage(video, 0, 0, 320, 240);
                     video.classList.add('hidden');
                     canvas.classList.remove('hidden');
-                    captureButton.textContent = 'Capturar desde Webcam';
+                    captureButton.textContent = 'Capture from Webcam';
                     stream.getTracks().forEach(track => track.stop());
-                    saveImage(); // Guardar la imagen
+                    saveImage(); // Save the image
                 };
             } catch (error) {
                 console.error('Error accessing webcam: ', error);
